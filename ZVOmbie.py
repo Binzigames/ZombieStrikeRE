39b67c24e5fa1f537b21e3befda6da606e1039fa
porko <127114283+Binzigames@users.noreply.github.com>
2025-01-25 19:24:03 +0200

    Animation fix

--- ZVOmbie.py
+++ ZVOmbie.py
@@ -6,15 +6,24 @@ class ZVOmbie:
         self.y = 500
         self.i = 0
         self.frame = 0
-        self.texture = pr.load_texture(f"Assets/game/enemy/enemyt.png")
+        self.textures = [pr.load_texture(f"Assets/game/enemy/Sprite-000{i}.png") for i in range(1, 5)]
 
     def Update(self):
         self.x -= 1
 
+        self.frame += 1
+        self.i = (self.frame // 10) % len(self.textures)
+
     def Draw(self):
-        pr.draw_texture_pro(self.texture, pr.Rectangle(0, 0, 29, 39), pr.Rectangle(self.x, self.y, 27, 39), pr.Vector2(0, 0), 0, pr.WHITE)
+        current_texture = self.textures[self.i]
+        pr.draw_texture_pro(
+            current_texture,
+            pr.Rectangle(0, 0, current_texture.width, current_texture.height),
+            pr.Rectangle(self.x, self.y, 27, 39),
+            pr.Vector2(0, 0),
+            0,  
+            pr.WHITE
+        )
 
     def NeNarkoman(self):
         pass
-    
-        
